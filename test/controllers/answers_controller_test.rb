c77a2b1896681bb4f27ddafd1cc449a48d3ca4f2
Tatiane Souza <tatianefx@gmail.com>
2015-11-23 11:24:53 -0200

    renamte answer test

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AnswerControllerTest < ActionController::TestCase
+class AnswersControllerTest < ActionController::TestCase
 	
   test "should get index" do
     get :index
