16e0f9d7babf16ff520e876658a95bb25392ad77
Tatiane Souza <tatianefx@gmail.com>
2015-11-23 00:51:33 -0200

    show data os table answer

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,7 +1,30 @@
 class AnswerController < ApplicationController
-  def create
+	def create
+  	@answer = Answer.new(answer_params)
+
+  	if @answer.save
+  		redirect_to action: :index
+  	else
+  		render :new
+  	end
   end
 
   def index
+  	@answers = Answer.all
+ 
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
+  end
+
+  def new
+  	@answer = Answer.new
+  end
+
+  private
+
+  def answer_params
+  	params.require(:answer).permit(:resident_id, :question_id, :text_, :apo_id, :room_id, :concept_id, :attribute_id, :synchronized)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+	get 'answer/create'
+
+  get 'answer/index'
+  
 	resources :upload, only: [:index, :create, :new]
   get '/', to:'home#index'
 
   resources :categories, only: [:index, :create, :new]
   get '/', to: 'home#index'
-
-  resources :answers, only: [:index, :create, :new, :show]
-  get '/', to:'home#index'
 end
